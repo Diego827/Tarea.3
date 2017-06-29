52c30628bf030af49a6f41dcaf01c90e3beb2823
Diego827 <dmarroquin827@gmail.com>
2017-06-28 18:28:28 -0600

    Mod Main
    Rama inicial de desarrollo

---
+++ src/umg/tarea3/main.java
@@ -0,0 +1,12 @@
+package umg.tarea3;
+
+/**
+ * Created by alumno on 28/06/2017.
+ */
+public class main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("test");
+    }
+}
